879f465e247fc399ba97af6aeafa3f2abfdaae3a
SebaPerez8 <sebaperez32@gmail.com>
2020-01-12 18:59:15 -0300

    commit final

---
+++ Formularios/ComprobanteRE.Designer.vb
@@ -0,0 +1,303 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ComprobanteRE
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ComprobanteRE))
+        Me.cmdCompRE = New System.Windows.Forms.Button()
+        Me.PrintDocument = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog = New System.Windows.Forms.PrintPreviewDialog()
+        Me.txtUsuario = New System.Windows.Forms.TextBox()
+        Me.DateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDomicilio = New System.Windows.Forms.TextBox()
+        Me.txtTelefono = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtProducto = New System.Windows.Forms.TextBox()
+        Me.txtObservaciones = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox = New System.Windows.Forms.PictureBox()
+        Me.cmdRemito = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.nudRemito = New System.Windows.Forms.NumericUpDown()
+        Me.nudComprobante = New System.Windows.Forms.NumericUpDown()
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudRemito, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.nudComprobante, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cmdCompRE
+        '
+        Me.cmdCompRE.FlatAppearance.BorderColor = System.Drawing.Color.Snow
+        Me.cmdCompRE.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdCompRE.Location = New System.Drawing.Point(553, 103)
+        Me.cmdCompRE.Name = "cmdCompRE"
+        Me.cmdCompRE.Size = New System.Drawing.Size(127, 58)
+        Me.cmdCompRE.TabIndex = 0
+        Me.cmdCompRE.Text = "Imprimir Comprobante "
+        Me.cmdCompRE.UseVisualStyleBackColor = True
+        '
+        'PrintDocument
+        '
+        '
+        'PrintPreviewDialog
+        '
+        Me.PrintPreviewDialog.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog.Document = Me.PrintDocument
+        Me.PrintPreviewDialog.Enabled = True
+        Me.PrintPreviewDialog.Icon = CType(resources.GetObject("PrintPreviewDialog.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog.Name = "PrintPreviewDialog"
+        Me.PrintPreviewDialog.Visible = False
+        '
+        'txtUsuario
+        '
+        Me.txtUsuario.Location = New System.Drawing.Point(56, 199)
+        Me.txtUsuario.Name = "txtUsuario"
+        Me.txtUsuario.Size = New System.Drawing.Size(158, 20)
+        Me.txtUsuario.TabIndex = 1
+        '
+        'DateTimePicker
+        '
+        Me.DateTimePicker.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.[Short]
+        Me.DateTimePicker.Location = New System.Drawing.Point(395, 59)
+        Me.DateTimePicker.Name = "DateTimePicker"
+        Me.DateTimePicker.Size = New System.Drawing.Size(129, 26)
+        Me.DateTimePicker.TabIndex = 2
+        Me.DateTimePicker.Value = New Date(2020, 1, 15, 0, 0, 0, 0)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(57, 177)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(58, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Cliente"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(548, 36)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(126, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "N° Comprobante"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(57, 226)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(72, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Domicilio"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(391, 36)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 20)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Fecha"
+        '
+        'txtDomicilio
+        '
+        Me.txtDomicilio.Location = New System.Drawing.Point(56, 249)
+        Me.txtDomicilio.Name = "txtDomicilio"
+        Me.txtDomicilio.Size = New System.Drawing.Size(158, 20)
+        Me.txtDomicilio.TabIndex = 9
+        '
+        'txtTelefono
+        '
+        Me.txtTelefono.Location = New System.Drawing.Point(274, 249)
+        Me.txtTelefono.Name = "txtTelefono"
+        Me.txtTelefono.Size = New System.Drawing.Size(158, 20)
+        Me.txtTelefono.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(274, 226)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(71, 20)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Teléfono"
+        '
+        'txtProducto
+        '
+        Me.txtProducto.Location = New System.Drawing.Point(41, 320)
+        Me.txtProducto.Multiline = True
+        Me.txtProducto.Name = "txtProducto"
+        Me.txtProducto.Size = New System.Drawing.Size(281, 100)
+        Me.txtProducto.TabIndex = 12
+        '
+        'txtObservaciones
+        '
+        Me.txtObservaciones.Location = New System.Drawing.Point(349, 320)
+        Me.txtObservaciones.Multiline = True
+        Me.txtObservaciones.Name = "txtObservaciones"
+        Me.txtObservaciones.Size = New System.Drawing.Size(323, 100)
+        Me.txtObservaciones.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(358, 293)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(137, 24)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Observaciones"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(62, 293)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(86, 24)
+        Me.Label7.TabIndex = 15
+        Me.Label7.Text = "Producto"
+        '
+        'PictureBox
+        '
+        Me.PictureBox.Image = CType(resources.GetObject("PictureBox.Image"), System.Drawing.Image)
+        Me.PictureBox.Location = New System.Drawing.Point(12, -1)
+        Me.PictureBox.Name = "PictureBox"
+        Me.PictureBox.Size = New System.Drawing.Size(364, 149)
+        Me.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox.TabIndex = 16
+        Me.PictureBox.TabStop = False
+        '
+        'cmdRemito
+        '
+        Me.cmdRemito.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdRemito.Location = New System.Drawing.Point(552, 236)
+        Me.cmdRemito.Name = "cmdRemito"
+        Me.cmdRemito.Size = New System.Drawing.Size(127, 63)
+        Me.cmdRemito.TabIndex = 17
+        Me.cmdRemito.Text = "Imprimir Remito"
+        Me.cmdRemito.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(572, 170)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(80, 20)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "N° Remito"
+        '
+        'nudRemito
+        '
+        Me.nudRemito.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nudRemito.Location = New System.Drawing.Point(552, 193)
+        Me.nudRemito.Maximum = New Decimal(New Integer() {999999, 0, 0, 0})
+        Me.nudRemito.Name = "nudRemito"
+        Me.nudRemito.Size = New System.Drawing.Size(120, 26)
+        Me.nudRemito.TabIndex = 19
+        Me.nudRemito.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.nudRemito.Value = New Decimal(New Integer() {500, 0, 0, 0})
+        '
+        'nudComprobante
+        '
+        Me.nudComprobante.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nudComprobante.Location = New System.Drawing.Point(552, 59)
+        Me.nudComprobante.Maximum = New Decimal(New Integer() {999999, 0, 0, 0})
+        Me.nudComprobante.Name = "nudComprobante"
+        Me.nudComprobante.Size = New System.Drawing.Size(120, 26)
+        Me.nudComprobante.TabIndex = 20
+        Me.nudComprobante.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        Me.nudComprobante.Value = New Decimal(New Integer() {15501, 0, 0, 0})
+        '
+        'ComprobanteRE
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(692, 455)
+        Me.Controls.Add(Me.nudComprobante)
+        Me.Controls.Add(Me.nudRemito)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.cmdRemito)
+        Me.Controls.Add(Me.PictureBox)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtObservaciones)
+        Me.Controls.Add(Me.txtProducto)
+        Me.Controls.Add(Me.txtTelefono)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtDomicilio)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateTimePicker)
+        Me.Controls.Add(Me.txtUsuario)
+        Me.Controls.Add(Me.cmdCompRE)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "ComprobanteRE"
+        Me.Text = "T&T"
+        CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudRemito, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.nudComprobante, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmdCompRE As Button
+    Friend WithEvents PrintDocument As Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog As PrintPreviewDialog
+    Friend WithEvents txtUsuario As TextBox
+    Friend WithEvents DateTimePicker As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtDomicilio As TextBox
+    Friend WithEvents txtTelefono As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtProducto As TextBox
+    Friend WithEvents txtObservaciones As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents PictureBox As PictureBox
+    Friend WithEvents cmdRemito As Button
+    Friend WithEvents Label8 As Label
+    Friend WithEvents nudRemito As NumericUpDown
+    Friend WithEvents nudComprobante As NumericUpDown
+End Class
---
+++ Formularios/ComprobanteRE.vb
@@ -0,0 +1,154 @@
+﻿Imports System.Drawing.Printing
+
+Public Class ComprobanteRE
+
+    Private Const NombreSeccion As String = "NumericValues"
+
+    Private Declare Function GetPrivateProfileString Lib "kernel32" Alias "GetPrivateProfileStringA" (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpDefault As String, ByVal lpReturnedString As String, ByVal nSize As Integer, ByVal lpFileName As String) As Integer
+    Private Declare Function WritePrivateProfileString Lib "kernel32" Alias "WritePrivateProfileStringA" (ByVal lpApplicationName As String, ByVal lpKeyName As String, ByVal lpString As String, ByVal lpFileName As String) As Integer
+
+    Dim Path_Archivo_Ini As String
+
+    Dim FUENTE As New Font("Arial", 16)
+    Dim FuenteFirma As New Font("Arial", 12)
+
+    Dim Firma As String = ""
+    Dim Observ As String = ""
+    Dim ObResult As String = ""
+    Dim CR As String = ""
+    Dim NComp As String = ""
+    Dim NRem As String = ""
+
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        Call GrabarINI(Path_Archivo_Ini, NombreSeccion, "nud1", nudComprobante.Value)
+        Call GrabarINI(Path_Archivo_Ini, NombreSeccion, "nud2", nudRemito.Value)
+
+    End Sub
+
+    Private Sub cmdCompRE_Click(sender As Object, e As EventArgs) Handles cmdCompRE.Click
+
+        NRem = ""
+        NComp = nudComprobante.Value
+
+
+        CR = "Comprobante de  
+Retiro / Entrega"
+        Observ = "OBSERVACIONES: "
+        ObResult = txtObservaciones.Text
+        Firma = ""
+        PrintPreviewDialog.ShowDialog()
+
+    End Sub
+
+
+    Private Sub PrintDocument_PrintPage(sender As Object, e As PrintPageEventArgs) Handles PrintDocument.PrintPage
+
+        Dim X As Integer = e.MarginBounds.Left
+        Dim Y As Integer = e.MarginBounds.Top
+
+        'Cliente
+
+        'Cabecera
+        e.Graphics.DrawImage(PictureBox.Image, X - 50, Y - 50)
+        e.Graphics.DrawString(CR, FUENTE, Brushes.Black, X + 220, Y)
+        e.Graphics.DrawString("N° " & NComp, FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y)
+        e.Graphics.DrawString("N° " & NRem, FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y)
+        e.Graphics.DrawString("Fecha: ", FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y + 30)
+        e.Graphics.DrawString(DateTimePicker.Text, FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y + 60)
+
+
+        e.Graphics.DrawString("-------------------------------------------------------------------------------------------", FUENTE, Brushes.Black, e.MarginBounds.Left - 30, Y + 100)
+
+
+        'Datos de cliente
+        e.Graphics.DrawString("Cliente: " & txtUsuario.Text, FUENTE, Brushes.Black, X - 30, Y + 130)
+        e.Graphics.DrawString("Domicilio: " & txtDomicilio.Text, FUENTE, Brushes.Black, X - 30, Y + 160)
+        e.Graphics.DrawString("Tel: " & txtTelefono.Text, FUENTE, Brushes.Black, e.MarginBounds.Right - 200, Y + 160)
+
+        'Producto
+        e.Graphics.DrawString("PRODUCTO: " & txtProducto.Text, FUENTE, Brushes.Black, X - 30, Y + 220)
+
+        'Observaciones
+        e.Graphics.DrawString(Observ & ObResult, FUENTE, Brushes.Black, X - 30, Y + 350)
+        e.Graphics.DrawString(Firma, FuenteFirma, Brushes.Black, e.MarginBounds.Right - 100, Y + 420)
+
+
+        'Negocio
+
+        'Cabecera
+        e.Graphics.DrawImage(PictureBox.Image, X - 50, Y + 500)
+        e.Graphics.DrawString(CR, FUENTE, Brushes.Black, X + 220, Y + 550)
+        e.Graphics.DrawString("N° " & NComp, FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y + 550)
+        e.Graphics.DrawString("N° " & NRem, FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y + 550)
+        e.Graphics.DrawString("Fecha: ", FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y + 580)
+        e.Graphics.DrawString(DateTimePicker.Text, FUENTE, Brushes.Black, e.MarginBounds.Right - 100, Y + 610)
+
+
+        e.Graphics.DrawString("-------------------------------------------------------------------------------------------", FUENTE, Brushes.Black, e.MarginBounds.Left - 30, Y + 650)
+
+
+        'Datos de cliente
+        e.Graphics.DrawString("Cliente: " & txtUsuario.Text, FUENTE, Brushes.Black, X - 30, Y + 680)
+        e.Graphics.DrawString("Domicilio: " & txtDomicilio.Text, FUENTE, Brushes.Black, X - 30, Y + 710)
+        e.Graphics.DrawString("Tel: " & txtTelefono.Text, FUENTE, Brushes.Black, e.MarginBounds.Right - 200, Y + 710)
+
+        'Producto
+        e.Graphics.DrawString("PRODUCTO: " & txtProducto.Text, FUENTE, Brushes.Black, X - 30, Y + 770)
+
+        'Observaciones
+
+        e.Graphics.DrawString(Observ & ObResult, FUENTE, Brushes.Black, X - 30, Y + 900)
+        e.Graphics.DrawString(Firma, FuenteFirma, Brushes.Black, e.MarginBounds.Right - 100, Y + 950)
+
+
+
+    End Sub
+
+
+    Private Sub cmdRemito_Click(sender As Object, e As EventArgs) Handles cmdRemito.Click
+
+        NComp = ""
+        NRem = nudRemito.Value
+
+        CR = "Remito"
+        Observ = ""
+        ObResult = ""
+        Firma = "Firma"
+        PrintPreviewDialog.ShowDialog()
+
+    End Sub
+
+    Private Sub ComprobanteRE_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        DateTimePicker.Text = Date.Today
+        Path_Archivo_Ini = Application.StartupPath & "\config.ini"
+
+        nudComprobante.Value = LeerINI(Path_Archivo_Ini, NombreSeccion, "nud1", 0)
+        nudRemito.Value = LeerINI(Path_Archivo_Ini, NombreSeccion, "nud2", 0)
+
+
+    End Sub
+
+    Private Function LeerINI(ByVal sFileName As String, ByVal sSection As String, ByVal sKeyName As String, Optional ByVal sDefault As String = "0") As String
+
+        Dim ret As Integer
+        Dim sRetVal As String
+
+        sRetVal = New String(Chr(0), 255)
+
+        ret = GetPrivateProfileString(sSection, sKeyName, sDefault, sRetVal, Len(sRetVal), sFileName)
+
+        If ret = 0 Then
+            Return sDefault
+        Else
+            Return Strings.Left(sRetVal, ret)
+        End If
+
+    End Function
+
+    Private Sub GrabarINI(ByVal sFileName As String, ByVal sSection As String, ByVal sKeyName As String, ByVal sValue As String)
+        Call WritePrivateProfileString(sSection, sKeyName, sValue, sFileName)
+    End Sub
+
+End Class
